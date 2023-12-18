4844cbc3044208ed9b9a1e967aa1cacd4d1f3888
Klaus Hoehfurtner <HoehfurtnerK190028@sus.htl-grieskirchen.at>
2023-12-18 13:48:07 +0100

    Slider only geaddet

---
+++ UCLib/Slideronly.xaml.cs
@@ -0,0 +1,45 @@
+﻿using DelegateLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UCLib
+{
+    /// <summary>
+    /// Interaction logic for Slideronly.xaml
+    /// </summary>
+    public partial class Slideronly : UserControl,MyUCInterface
+    {
+        public Slideronly()
+        {
+            InitializeComponent();
+        }
+
+        public MyModel UCModel { get; set; }
+        
+
+        private void SldValue_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            UCModel.ModelValue = Math.Round(e.NewValue, 0);
+        }
+
+        public void ValueChangedFromOutside(object sender, MyEventArgs args)
+        {
+            if (UCModel != null)
+            {
+                sldValue.Value = args.Value;
+            }
+        }
+    }
+}
